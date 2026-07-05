c7d77e000652d60b1953d673e2b010a87988b800
Alexey Budai <alexeybudai@gmail.com>
2026-07-05 14:29:38 +0300

    Add front

---
+++ orval.config.ts
@@ -0,0 +1,27 @@
+// orval.config.ts
+import { defineConfig } from "orval";
+
+export default defineConfig({
+  api: {
+    input: {
+      target: "http://localhost:3000/swagger-json",
+    },
+    output: {
+      mode: "split",
+      target: "./src/api/hooks.ts",
+      schemas: "./src/api/models",
+      operationSchemas: "./src/api/operation-types",
+      client: "react-query",
+      // 👇 Настройка базового URL
+      baseUrl: {
+        runtime: "env.backendUrl",
+        imports: [
+          {
+            name: "env",
+            importPath: "@/config/env",
+          },
+        ],
+      },
+    },
+  },
+});
