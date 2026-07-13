04c947ca5e5a58e359191d4da6db08af56fec31a
Alexey Budai <alexeybudai@gmail.com>
2026-07-14 00:06:45 +0300

    добавление "Подробнее" для карточки товара

--- src/app/catalog/(product)/[id]/page.tsx
+++ src/app/catalog/(product)/[id]/page.tsx
@@ -1,4 +1,29 @@
+import type { Metadata } from "next";
 import ProductPage from "@/app/catalog/(product)/[id]/productPage";
+import { products } from "@/data/products";
+
+type Props = {
+  params: Promise<{ id: string }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { id } = await params;
+
+  const product = products.find((p) => p.id === Number(id));
+
+  return {
+    title: `My Shop | ${product?.name}`,
+    description: product?.description,
+    openGraph: {
+      type: "website",
+      title: product?.name,
+      description: product?.description,
+      images: [product?.image ?? ""],
+      siteName: "My Shop",
+    },
+  };
+}
 
 const Page = () => {
   return (
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -18,43 +18,52 @@ export const ProductCard = ({ product }: ProductCardProps) => {
   const currentSrc = hasError ? FALLBACK_IMAGE : product.image;
 
   return (
-    <Link
-      className="flex flex-col gap-1"
-      href={`catalog/${product.id}`}
-      scroll={false}
-    >
-      <div className="relative w-50 h-50 overflow-hidden bg-gray-100">
-        {isLoading && <Spinner />}
-        <Image
-          src={currentSrc} // Динамический src
-          alt={product.name}
-          width={200}
-          height={200}
-          className="object-cover w-full h-full"
-          sizes="200px"
-          unoptimized
-          onLoad={() => setIsLoading(false)}
-          onError={() => {
-            if (!hasError) {
-              setHasError(true);
-              setIsLoading(false);
-            }
-          }}
-        />
-      </div>
-      <div className="product-info">
-        <span className="product-category">{product.category}</span>
-        <h3 className="product-name">{product.name}</h3>
-        <p className="product-description">{product.description}</p>
-        <div className="product-footer">
-          <span className="product-price">
-            {product.price.toLocaleString("ru-RU")} ₽
-          </span>
-          <button type="button" className="add-to-cart-btn">
-            В корзину
-          </button>
+    <div>
+      <Link
+        className="flex flex-col gap-1"
+        href={`catalog/${product.id}`}
+        scroll={false}
+      >
+        <div className="relative w-50 h-50 overflow-hidden bg-gray-100">
+          {isLoading && <Spinner />}
+          <Image
+            src={currentSrc} // Динамический src
+            alt={product.name}
+            width={200}
+            height={200}
+            className="object-cover w-full h-full"
+            sizes="200px"
+            unoptimized
+            onLoad={() => setIsLoading(false)}
+            onError={() => {
+              if (!hasError) {
+                setHasError(true);
+                setIsLoading(false);
+              }
+            }}
+          />
         </div>
-      </div>
-    </Link>
+        <div className="product-info">
+          <span className="product-category">{product.category}</span>
+          <h3 className="product-name">{product.name}</h3>
+          <p className="product-description">{product.description}</p>
+          <div className="product-footer">
+            <span className="product-price">
+              {product.price.toLocaleString("ru-RU")} ₽
+            </span>
+            <button type="button" className="add-to-cart-btn">
+              В корзину
+            </button>
+          </div>
+        </div>
+      </Link>
+      <Link
+        href={`catalog/${product.id}`}
+        target={"_blank"}
+        className={"text-blue-500 hover:underline hover:text-blue-950"}
+      >
+        Подробнее
+      </Link>
+    </div>
   );
 };
